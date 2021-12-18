ef50d643c2f0bd502cab53adbba01e5d22f024db
nicola <nicola.lazzarin89@gmail.com>
2021-12-18 11:55:10 +0100

    update database

--- database.sql
+++ database.sql
@@ -1,42 +1,28 @@
-aggiungere stelle per le recensioni
-
-CREATE TABLE userInfo(
-    email               VARCHAR(50)     NOT NULL        CHECK (Email LIKE '%_@__%.__%'),
-    nome                VARCHAR(30)     NOT NULL        
-    cognome             VARCHAR(30)     NOT NULL
-    paese               VARCHAR(30)     DEFAULT NULL,
-    CAP                 VARCHAR(10)     DEFAULT NULL,
-      
-    PRIMARY KEY(Email)
-);   
-
-CREATE TABLE user(
-    username            VARCHAR(30),
-    email               VARCHAR(50)     UNIQUE          NOT NULL    CHECK (Email LIKE '%_@__%.__%'),
-    _password            VARCHAR(30)     NOT NULL,                   
-
-    PRIMARY KEY (Username),
-    FOREIGN KEY (Email) REFERENCES userInfo(Email) ON UPDATE CASCADE ON DELETE CASCADE
+CREATE TABLE `user`(
+    `email` VARCHAR(255) NOT NULL,
+    `username` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `name` VARCHAR(255) NULL,
+    `surname` VARCHAR(255) NULL,
+    `city` VARCHAR(255) NULL,
+    `isAdmin` TINYINT(1) NOT NULL
 );
+ALTER TABLE
+    `user` ADD PRIMARY KEY `user_email_primary`(`email`);
+ALTER TABLE
+    `user` ADD UNIQUE `user_username_unique`(`username`);
 
-CREATE TABLE `admin`(
-    username            VARCHAR(30),
-    numTelefono         VARCHAR(15)     DEFAULT NULL,
-    email               VARCHAR(50)     UNIQUE          NOT NULL    CHECK (Email LIKE '%_@__%.__%'),
-    `password`            VARCHAR(30)     NOT NULL,
-
-    PRIMARY KEY (username)
-);  
-
-CREATE TABLE commento(
-    idCommento          AUTO_INCREMENT,
-    membro              VARCHAR(30)		 NOT NULL,
-    nStelle             TINYINT          NOT NULL,
-    commento            VARCHAR(2048)    NOT NULL,
-    dataC               DATE             DEFAULT current_date,
-    `admin`              VARCHAR(30)      DEFAULT NULL,
- 
-    PRIMARY KEY (idCommento),
-    FOREIGN KEY (`admin`) REFERENCES _admin(Username) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (user) REFERENCES user(Username) ON UPDATE CASCADE ON DELETE CASCADE,
-); 
+CREATE TABLE `comment`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(255) NOT NULL,
+    `comment` VARCHAR(255) NOT NULL,
+    `stars` TINYINT UNSIGNED NOT NULL,
+    `date` DATETIME NOT NULL,
+    `isBanned` VARCHAR(255) NULL
+);
+ALTER TABLE
+    `comment` ADD PRIMARY KEY `comment_id_primary`(`id`);
+ALTER TABLE 
+    `user` ADD FOREIGN KEY (`username`) REFERENCES `comment` (`username`);
+ALTER TABLE 
+    `comment` ADD FOREIGN KEY (`isBanned`) REFERENCES `user` (`username`);
