4d6d1bb989da816577e9342a8ed3cd83b2afb922
nicola <nicola.lazzarin89@gmail.com>
2022-01-06 11:18:08 +0100

    parte di php funzionante, aggiunto al database degli user

--- database.sql
+++ database.sql
@@ -1,3 +1,6 @@
+DROP TABLE IF EXISTS `comment`;
+DROP TABLE IF EXISTS `user`;
+
 CREATE TABLE `user`(
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(255) NOT NULL,
@@ -24,6 +27,23 @@ CREATE TABLE `comment`(
 ALTER TABLE
     `comment` ADD CONSTRAINT `comment_userid_foreign` FOREIGN KEY(`userId`) REFERENCES `user`(`id`);
 
+
+INSERT INTO
+`user` (`id`, `email`,`username`, `password`, `name`, `surname`, `city`, `isAdmin`)
+values
+(NULL, 'admin@admin.com','admin', 'admin', 'genoveffo', 'paloduro', 'skskucity', 1),
+(NULL, 'user@user.com','user', 'user', 'franco', 'battiatino', 'cuccuruccucù', 0),
+(NULL, 'giovanni@mail.com','giovanni', 'giovanni', 'giovanni', 'giovannini', 'giovannopoli', 0),
+(NULL, 'francesco@mail.com','francesco', 'francesco', 'francesco', 'franceschini', 'francescopoli', 0),
+(NULL, 'giulio@mail.com','giulio', 'giulio', 'giulio', 'giulini', 'giulienopoli', 0);
+
+INSERT INTO
+`comment` (`id`, `userId`, `comment`, `stars`, `date`) 
+values 
+(NULL, 2, 'Bellssimo impianto! costa 800 mila euro uno skipass però ne vale la pena!', 5, NOW()),
+(NULL, 3, '800 mila euro per uno skipass? neanche a cortina costa così tanto!', 2, NOW()),
+(NULL, 4, 'Che schifo!, le piste sono battuta male e gli impianti di risalita sono pieni di barboni! orrore!', 1, NOW()),
+(NULL, 5, 'Fanc*lo sto skifo di pise, nn ci o mai scato pero dalle recenzioni dicono che fa skifo quindi fa skifo!', 1, NOW());
 
 
 
