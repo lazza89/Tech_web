6afeb13fd240ab1d98c2256454e80105a9491aa2
nicola <nicola.lazzarin89@gmail.com>
2022-01-21 19:28:54 +0100

    templetizzato le pagine eventi, home, mappa, servizi e tariffe

--- php/servizi.php
+++ php/servizi.php
@@ -1,26 +1,10 @@
 <?php
+include 'template/logged.php';
 if(!isset($_SESSION)) {
     session_start();
 }
 
-$HTMLPage = file_get_contents("../html/servizi.html");
-
-if (isset($_SESSION['login']) && $_SESSION['login']) { 
-    $username = $_SESSION['username'];
-    $HTMLPage = str_replace("{{logged}}", "Ciao $username! " , $HTMLPage);
-    $HTMLPage = str_replace("{{loginPage}}", "logout.php" , $HTMLPage);
-    $HTMLPage = str_replace("{{login}}", "Logout" , $HTMLPage);
-    $HTMLPage = str_replace("{{areaPersonale}}", "<li><a href=\"areaPersonale.php\">Profilo</a></li>" , $HTMLPage);
-
-}else{
-    $HTMLPage = str_replace("{{logged}}", "" , $HTMLPage);
-    $HTMLPage = str_replace("{{loginPage}}", "login.php" , $HTMLPage);
-    $HTMLPage = str_replace("{{login}}", "Login" , $HTMLPage);
-    $HTMLPage = str_replace("{{areaPersonale}}", "" , $HTMLPage);
-}
-
-$HTMLPage = str_replace("{{footer}}", file_get_contents("../html/components/footer.html") , $HTMLPage);
-
+$HTMLPage = isLogged("../html/servizi.html");
 
 echo $HTMLPage;
 ?>
